395bbe7729eaa2eac582328bd60740a2ea990689
Zhifeng <tzephons@gmail.com>
2020-09-08 21:46:27 +0200

    Change authorities

--- src/main/java/fr/isika/projet4/ServerSpringBoot/constant/AuthorityConstant.java
+++ src/main/java/fr/isika/projet4/ServerSpringBoot/constant/AuthorityConstant.java
@@ -2,7 +2,7 @@ package fr.isika.projet4.ServerSpringBoot.constant;
 
 public class AuthorityConstant {
 	
-	public static final String[] USER_AUTHORITIES = { "analysis", "user:read", "user:create", "user:update", "user:delete" };
+	public static final String[] USER_AUTHORITIES = { "analysis", "user:read" };
 	public static final String[] ASSISTANT_AUTHORITIES = { "user:read", "user:update" };
     public static final String[] ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update" };
     public static final String[] SUPER_ADMIN_AUTHORITIES = { "user:read", "user:create", "user:update", "user:delete" };
